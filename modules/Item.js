9e7ed9929342f785880e84ecbca93467631a8c3c
Jacob Archambault <jacob.archambault@waystar.com>
2021-03-28 12:33:34 -0400

    Change db name to cars4sale

--- modules/Item.js
+++ modules/Item.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/makewaves');  // db name
+mongoose.connect('mongodb://localhost:27017/cars4sale');  // db name
 
 var Schema = mongoose.Schema;
 
