eceb0774f23b18ac1a30c22e0cf1a95180ceb565
Jacob Archambault <jacob.archambault@waystar.com>
2021-03-28 15:02:13 -0400

    Update item Schema

--- modules/Item.js
+++ modules/Item.js
@@ -5,9 +5,13 @@ mongoose.connect('mongodb://localhost:27017/cars4sale');  // db name
 var Schema = mongoose.Schema;
 
 var itemSchema = new Schema({
-	name: {type: String, required: true, unique: true},
-	quantity: {type: Number, min: 0, required: true},
-	cost: { type: Number, min: 0, required: true },
+	cid: {type: Number, unique: true},
+	year: {type: Number },
+	make: {type: String},
+    model: {type: String},
+    miles: {type: Number},
+    price: { type: Number, min: 0, required: true },
+    dealer_id: {type: String}
 });
 
 module.exports = mongoose.model('Item', itemSchema);
